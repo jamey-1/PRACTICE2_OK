44842eda957b73db1441012109d0b625491fcfd0
YEMING LIU <liuyeming@zhouyang.net>
2022-06-28 11:54:24 -0600

    first commit

---
+++ src/main/java/org/comit/App.java
@@ -0,0 +1,44 @@
+package org.comit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi, I already print sentences." );
+        System.out.println("*********************************");
+        
+        int i1 = 273;
+        int i2 = 597;
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println("*********************************");
+        
+        double d1 = 5.3;
+        double d2 = 7.5;
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println("*********************************");
+        
+        int a = 1234;
+        int b = 532;
+        int sum = a + b;
+        System.out.println("Sum is: " + sum);
+        System.out.println("*********************************");
+        
+        int sub = a - b;
+        System.out.println("Sub is: " + sub);
+        System.out.println("*********************************");
+        
+        int mul = a * b;
+        System.out.println("Mul is: " + mul);
+        System.out.println("*********************************");
+        
+        double div = (double)a / b;
+        System.out.println("Div is: " + div);
+        System.out.println("*********************************");
+    }
+}
