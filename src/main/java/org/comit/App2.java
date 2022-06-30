7361574562bb64065830f9e2077002018648a095
YEMING LIU <liuyeming@zhouyang.net>
2022-06-30 10:55:17 -0600

    add question 8 to 15

---
+++ src/main/java/org/comit/App2.java
@@ -0,0 +1,48 @@
+package org.comit;
+
+public class App2 {
+	
+	public static void main(String[] args) {
+		
+//		8 - Write a program in Java that prints the numbers 1 to 3 on the screen.
+//		for(int i = 1; i < 4; i++) {
+//			System.out.println(i);
+//		}
+		
+//		9 - Write a program in Java that prints the numbers 1 to 9 on the screen.
+//		for(int i = 1; i < 10; i++) {
+//			System.out.println(i);
+//		}
+		
+//		10 - Write a program in Java that prints on the screen the numbers from 1 to 10,000.
+//		for(int i = 1; i < 10_001; i++) {
+//			System.out.println(i);
+//		}
+		
+//		11 - Write a program in Java that prints the numbers from 5 to 10 on the screen.
+//		for(int i = 5; i < 11; i++) {
+//			System.out.println(i);
+//		}
+		
+//		12 - Write a program in Java that prints the numbers from 5 to 15 on the screen.
+//		for(int i = 5; i < 16; i++) {
+//			System.out.println(i);
+//		}
+		
+//		13 - Write a program in Java that prints on the screen the numbers from 5 to 15,000.
+//		for(int i = 5; i < 15_001; i++) {
+//			System.out.println(i);
+//		}
+		
+//		14 - Write a program in Java that prints 200 times the word "hello". Note: in the source code that you write you must only enter once the word "hello".
+//		for(int i = 0; i < 200; i++) {
+//			System.out.println("hello");
+//		}
+		
+//		15 - Exercise: Write a program in Java that prints the squares of the first 30 natural numbers on the screen.
+		for(int i = 1; i <= 30; i++) {
+			System.out.println(i * i);
+		}
+	}
+	
+}
